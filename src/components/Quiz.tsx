4c576eeb8a41f3fc6a863288f852d11fa63380dd
Navdeep Singh <navdeep.er@gmail.com>
2020-12-29 14:22:13 +0530

    WIP

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -14,90 +14,115 @@ export type AnswerObject = {
   correctAnswer: string
 }
 
+type IStateProps = {
+  loading: boolean
+  number: number
+  score: number
+  gameOver: boolean
+  questions: QuestionState[]
+  userAnswers: AnswerObject[]
+}
+
 const TOTAL_QUESTIONS = 10
 
 export const Quiz = () => {
-  const [loading, setLoading] = React.useState(false)
-  const [questions, setQuestions] = React.useState<QuestionState[]>([])
-  const [number, setNumber] = React.useState(0)
-  const [userAnswers, setUserAnswers] = React.useState<AnswerObject[]>([])
-  const [score, setScore] = React.useState(0)
-  const [gameOver, setGameOver] = React.useState(true)
+  const [state, setState] = React.useState<IStateProps>({
+    loading: false,
+    number: 0,
+    score: 0,
+    gameOver: true,
+    questions: [],
+    userAnswers: [],
+  })
+
+  React.useEffect(() => {
+    console.log('Re-rendered')
+  })
 
   const startTrivia = async () => {
-    setLoading(true)
-    setGameOver(false)
+    setState({ ...state, loading: true })
 
     const newQuestions = await fetchQuizQuestions(
       TOTAL_QUESTIONS,
       Difficulty.EASY,
     )
 
-    setQuestions(newQuestions)
-    setScore(0)
-    setUserAnswers([])
-    setNumber(0)
-    setLoading(false)
+    setState({
+      ...state,
+      gameOver: false,
+      questions: newQuestions,
+      score: 0,
+      loading: false,
+      number: 0,
+      userAnswers: [],
+    })
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (!gameOver) {
+    if (!state.gameOver) {
       // Users answer
       const answer = e.currentTarget.value
+
       // Check answer against correct answer
-      const correct = questions[number].correct_answer === answer
+      const correct = state.questions[state.number].correct_answer === answer
       e.currentTarget.classList.add(correct ? 'correct' : 'incorrect')
-      // Add score if answer is correct
-      if (correct) setScore(prev => prev + 1)
+
       // Save answer in the array for user answers
       const answerObject = {
-        question: questions[number].question,
+        question: state.questions[state.number].question,
         answer,
         correct,
-        correctAnswer: questions[number].correct_answer,
+        correctAnswer: state.questions[state.number].correct_answer,
       }
 
-      setUserAnswers(prev => [...prev, answerObject])
+      setState({
+        ...state,
+        userAnswers: [...state.userAnswers, answerObject],
+        score: correct ? state.score + 1 : state.score,
+      })
     }
   }
 
   const nextQuestion = () => {
     // Move on to the next question if not the last question
-
-    const nextQuestion = number + 1
+    const nextQuestion = state.number + 1
 
     if (nextQuestion === TOTAL_QUESTIONS) {
-      setGameOver(true)
+      setState({ ...state, gameOver: true })
     } else {
-      setNumber(nextQuestion)
+      setState({ ...state, number: nextQuestion })
     }
   }
   return (
     <>
-      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+      {state.gameOver || state.userAnswers.length === TOTAL_QUESTIONS ? (
         <button className={styles.start} onClick={startTrivia}>
           START
         </button>
       ) : null}
 
-      {!gameOver ? <p className={styles.score}>Score: {score}</p> : null}
+      {!state.gameOver ? (
+        <p className={styles.score}>Score: {state.score}</p>
+      ) : null}
 
-      {loading && <p>Loading Questions...</p>}
+      {state.loading && <p>Loading Questions...</p>}
 
-      {!loading && !gameOver && (
+      {!state.loading && !state.gameOver && (
         <QuestionCard
-          questionNumber={number + 1}
+          questionNumber={state.number + 1}
           totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number].question}
-          answers={questions[number].answers}
-          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          question={state.questions[state.number].question}
+          answers={state.questions[state.number].answers}
+          userAnswer={
+            state.userAnswers ? state.userAnswers[state.number] : undefined
+          }
           callback={checkAnswer}
         />
       )}
 
-      {!gameOver &&
-      userAnswers.length === number + 1 &&
-      number !== TOTAL_QUESTIONS - 1 ? (
+      {!state.gameOver &&
+      state.userAnswers.length === state.number + 1 &&
+      state.number !== TOTAL_QUESTIONS - 1 ? (
         <button className={styles.next} onClick={nextQuestion}>
           NEXT QUESTION
         </button>
